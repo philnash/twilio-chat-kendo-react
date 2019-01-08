bc3ed89a221e6e7e34eaf939868f168f3c016142
Phil Nash <philnash@gmail.com>
2019-01-08 14:56:54 +1100

    Working Login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import Login from './Login';
 import '@progress/kendo-theme-default/dist/all.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      loggedIn: false
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+  }
+
+
+  handleLogin(event) {
+    event.preventDefault();
+    this.setState({ loggedIn: true });
+  }
+  handleUsernameChange(event) {
+    this.setState({ username: event.target.value });
   }
 
   render() {
-    return <p>Hello world</p>;
+    let loginOrChat;
+    if (this.state.loggedIn) {
+      loginOrChat = <p>Logged in as {this.state.username}</p>;
+    } else {
+      loginOrChat = (
+        <Login
+          handleLogin={this.handleLogin}
+          handleUsernameChange={this.handleUsernameChange}
+          username={this.state.username}
+        />
+      );
+    }
+    return (
+      <div className="container">
+        <div className="row mt-3 justify-content-center">{loginOrChat}</div>
+      </div>
+    );
   }
 }
 
